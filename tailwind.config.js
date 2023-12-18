847cba951ecafade3e110126878d1606bd0ae4fa
Arafat Jamil <arafatjamil01@gmail.com>
2023-12-19 02:58:32 +0600

    Removed tailwind, added support and documentation links, created pot file

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.php", "./**/*.php", "./**/**/*.php", "./**/**/**/*.php", "./**/**/**/**/*.php"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
